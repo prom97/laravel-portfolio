37d1ec05b2a1b1a22a09513c3c31ec22243e4441
Pablo Damian Romero <pablo.romero@est.fi.uncoma.edu.ar>
2022-07-18 21:39:25 -0300

    factory & seeders implemented (projects)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +15,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'mail@admin.com',
+            'password' => Hash::make('admin123')
+        ]);
+        
+        foreach(range(1,50) as $index) 
+         {
+            \App\Models\Project::factory()->create([
+                'url' => 'proyecto-' . $index,
+                'title' => 'Proyecto n° ' . $index
+            ]);
+         }
     }
 }
